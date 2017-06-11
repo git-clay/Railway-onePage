d59c4f1a1658a99bd465a06d1679550384c2cf7b
git-clay <ce.smith5525@gmail.com>
2017-06-11 12:21:15 -0600

    fixed weird wrapping issue in programs

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function( $ ) {
 
   // Preloader
   $(window).on('load', function() {
-    $('#preloader').delay(100).fadeOut('slow',function(){$(this).remove();});
+    $('#preloader').delay(1000).fadeOut('slow',function(){$(this).remove();});
   });
 
   // Hero rotating texts
